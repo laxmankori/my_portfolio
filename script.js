fa118848c3c6aad21d6c66116442b54be57803ef
laxmankori <101863822+laxmankori@users.noreply.github.com>
2024-01-31 20:15:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+function menuOpen(){
+let nav = document.getElementById("nav");
+let link = document.getElementById("link");
+
+if (nav.style.height === "60vw") {
+    nav.style.height = "5rem";
+    nav.style.transition = "all ease 0.5s";
+    link.style.display = "none"
+    
+}else{
+    nav.style.height = "60vw";
+    nav.style.transition = "all ease 0.5s";
+    link.style.display = "block"
+
+}
+// nav.style.height = "60vw";
+nav.style.alignItems = "baseline";
+nav.style.paddingTop = "5vw";
+// nav.style.height = "15vw"
+
+}
+
+function changeIcon(x) {
+    x.classList.toggle("fa-xmark");
+    // document.getElementById("nav").style.height = "15vw";
+}
+
+// Stciky navbar
+
+const section_hero = document.querySelector(".container");
+
+const observer = new IntersectionObserver((entries) => 
+{
+    const ent = entries[0];
+    console.log(ent);
+    ent.isIntersecting === false ? nav.classList.add('sticky_navbar'): nav.classList.remove('sticky_navbar')
+},
+{
+    root: null,
+    rootMargin: "-80px",
+    threshold: 0.5,
+}
+);
+
+observer.observe(section_hero);
