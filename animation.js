2f6f5823fd37f54dc73a61925119499c4432df34
laxmankori <101863822+laxmankori@users.noreply.github.com>
2024-06-27 12:56:53 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,1 +1,31 @@
+var tl = gsap.timeline();
+
+tl.from(".logo h1,#link ul li,#nav__btn",{
+    y:-20,
+    duration: 0.7,
+    delay:0.5,
+    opacity:0,
+    stagger:0.15
+})
+
+
+
+tl.from(".header_content h1",{
+    x:200,
+    duration:1,
+    opacity:0,
+    stagger:0.15
+})
+
+tl.from(".header_logo img",{
+    opacity:0,
+    duration:0.5,
+},"-=1")
+
+tl.from(".design, .massage,.phone_no",{
+    y:300,
+    duration:1,
+    opacity:0,
+    stagger:0.15
+},"-=1")
 
