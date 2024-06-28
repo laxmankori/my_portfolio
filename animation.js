3044db0ef1344c9c6cb5597dc32befbf2fb9d39f
laxmankori <101863822+laxmankori@users.noreply.github.com>
2024-06-28 14:58:02 +0530

    Update animation.js
    page2 animation add

--- animation.js
+++ animation.js
@@ -1,4 +1,6 @@
-var tl = gsap.timeline();
+function page1Animation() {
+    var tl = gsap.timeline();
+
 
 tl.from(".logo h1,#link ul li,#nav__btn",{
     y:-20,
@@ -29,3 +31,46 @@ tl.from(".design, .massage,.phone_no",{
     stagger:0.15
 },"-=1")
 
+
+}
+
+
+function page2Animation() {
+    var tl2 = gsap.timeline({scrollTrigger:{
+        trigger:".experties",
+        scroller:"body",
+        start:"top 50%",
+        end:"top 0%",
+        scrub:2,
+    }});
+    
+    tl2.from(".experties_heading h3",{
+        y:30,
+        duration:0.5,
+        opacity:0
+    })
+    
+    tl2.from(".pro1__left",{
+        x:-50,
+        duration:1,
+        opacity:0
+    },"pro1")
+    tl2.from(".pro2__right",{
+        x:50,
+        duration:1,
+        opacity:0
+    },"pro1")
+    tl2.from(".pro3__left",{
+        x:-50,
+        duration:1,
+        opacity:0
+    },"pro2")
+    tl2.from(".pro4__right",{
+        x:50,
+        duration:1,
+        opacity:0
+    },"pro2")
+}
+
+page1Animation();
+page2Animation();
